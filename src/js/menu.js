6104b9df186e3378817e654e97f0fff5690f8e77
Juan Felipe <vargasjuan367@gmail.com>
2024-05-12 11:50:56 -0500

    se refactoriza el menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,56 +1,91 @@
 $(document).ready(function(){
 	document.querySelector("#sidebar").classList.toggle("expand");
-	$("#contentCarta").show();
-	$("#contentEventos").hide();
-	$("#contentReserva").hide();
-	$("#contentContacto").hide();
-	$("#contentHistoria").hide();
+	
+	mostrarCapas("contentCarta")
+	ocultarCapas("contentEventos");
+	ocultarCapas("contentReserva");
+	ocultarCapas("contentContacto");
+	ocultarCapas("contentHistoria");
+	ocultarCapas("contenedorubicacion");
 
     $("#pageCarta").on('click', function(){
+    	$("#contenedorubicacion").hide();
     	$("#contentEventos").hide();
 		$("#contentReserva").hide();
 		$("#contentContacto").hide();
 		$("#contentHistoria").hide();
-    	$("#contentCarta").show();
-    });
-	$("#home").on('click', function(){
-    	$("#contentCarta").hide();
-		$("#contentReserva").hide();
-		$("#contentContacto").hide();
-		$("#contentHistoria").hide();
-    	$("#contentEventos").hide();
+    	mostrarCapas("contentCarta");
     });
     $("#pageEventos").on('click', function(){
+    	$("#contenedorubicacion").hide();
     	$("#contentCarta").hide();
 		$("#contentReserva").hide();
 		$("#contentContacto").hide();
 		$("#contentHistoria").hide();
-    	$("#contentEventos").show();
+    	mostrarCapas("contentEventos");
     });
     $("#pageReserva").on('click', function(){
+    	$("#contenedorubicacion").hide();
     	$("#contentEventos").hide();
 		$("#contentContacto").hide();
 		$("#contentHistoria").hide();
 		$("#contentCarta").hide();
-    	$("#contentReserva").show();
+    	mostrarCapas("contentReserva");
     });
     $("#pageContacto").on('click', function(){
+    	$("#contenedorubicacion").hide();
     	$("#contentEventos").hide();
 		$("#contentReserva").hide();
 		$("#contentCarta").hide();
 		$("#contentHistoria").hide();
-    	$("#contentContacto").show();
+    	mostrarCapas("contentContacto");
     });
     $("#PageHistoria").on('click', function(){
+    	$("#contenedorubicacion").hide();
     	$("#contentEventos").hide();
 		$("#contentReserva").hide();
 		$("#contentContacto").hide();
 		$("#contentCarta").hide();
-    	$("#contentHistoria").show();
+    	mostrarCapas("contentHistoria");
+    });
+    $("#ubicacion").on('click', function(){
+    	$("#contentEventos").hide();
+		$("#contentReserva").hide();
+		$("#contentContacto").hide();
+		$("#contentCarta").hide();
+		$("#contentHistoria").hide();
+		mostrarCapas("contenedorubicacion");
     });
 });
-const hamBurger = document.querySelector(".toggle-btn");
 
+const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
-   document.querySelector("#sidebar").classList.toggle("expand");
+
+	let contenedorMenu, sidebar, contenidoPagina;
+
+	contenedorMenu = document.getElementById("main");
+	sidebar = document.querySelector("#sidebar").classList;
+	document.querySelector("#sidebar").classList.toggle("expand");
+
+	//contenidoPagina	
+
+
+	if (sidebar.value == "expand") {
+		contenedorMenu.style.marginLeft = "18%";
+		contenedorMenu.style.width = "82%";
+		
+	} else {
+		contenedorMenu.style.marginLeft = "6%";
+		contenedorMenu.style.width = "99%";
+	}
+   
 });
+
+
+function ocultarCapas(elemento){
+	$("#"+elemento).hide();
+}
+
+function mostrarCapas(elemento){
+	$("#"+elemento).show();
+}
